4f96f0ed8e5ebdaf3ea25fb3821248a808c9ef5d
Cristiano <cristianovelkan@gmail.com>
2019-12-06 16:34:32 -0300

    purge apache2 and add -y

--- full-stack.sh
+++ full-stack.sh
@@ -1,19 +1,27 @@
+#remove apache2
+sudo apt-get remove apache2 -y
+sudo apt-get purge apache2 -y
+sudo apt autoremove -y
+
+sudo rm /var/www/html/index.html
+
+#update
 sudo apt-get update
 sudo apt install build-essential -y
 
 # Install Nginx
-sudo apt-get install nginx
+sudo apt-get install nginx -y
 
 # Install PHP 7.2
 sudo apt-get update
-sudo apt-get install php7.2
+sudo apt-get install php7.2 -y
 
 # Install needed modules for PHP
-sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip php-bcmath
+sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip php-bcmath -y
 
 # Install Composer (PHP dependencies manager)
 ## First install php-cli, unzip, git, curl, php-mbstring
-sudo apt-get install curl git unzip
+sudo apt-get install curl git unzip -y
 
 ## Downloading and installing Composer
 cd ~
@@ -25,6 +33,8 @@ sudo systemctl reload nginx
 #node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
+source ~/.bashrc
+
 nvm install v12
 nvm alias default v12
 
@@ -32,4 +42,4 @@ nvm alias default v12
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 
-sudo apt-get install python-certbot-nginx
+sudo apt-get install python-certbot-nginx -y
