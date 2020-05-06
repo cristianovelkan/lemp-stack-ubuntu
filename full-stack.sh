fd5e4e0ae95140b4b5c1f003bab4236fb24e43f4
Cristiano Mendes <30302121+cristianovelkan@users.noreply.github.com>
2020-05-06 14:55:38 -0300

    Update full-stack.sh
    update to php7.4

--- full-stack.sh
+++ full-stack.sh
@@ -1,16 +1,18 @@
 #update
 sudo apt-get update
 sudo apt install build-essential -y
+sudo apt -y install software-properties-common
+
+# Install PHP 7.4
+sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/nginx-mainline
+sudo apt-get update
 
 # Install Nginx
-sudo apt-get install nginx -y
-
-# Install PHP 7.2
-sudo apt-get update
-sudo apt-get install php7.2 -y
+sudo apt-get install nginx php7.4-fpm
 
 # Install needed modules for PHP
-sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip php-bcmath -y php7.2-gmp php7.2-intl
+sudo apt-get install php7.4-mysql php7.4-curl php7.4-gd php7.4-bz2 php7.4-mbstring php7.4-xml php7.4-zip php-bcmath -y php7.4-gmp php7.4-intl
 
 # Install Composer (PHP dependencies manager)
 ## First install php-cli, unzip, git, curl, php-mbstring
@@ -25,17 +27,17 @@ sudo apt-get remove apache2 -y
 sudo apt-get purge apache2 -y
 sudo apt autoremove -y
 
-sudo apt-get purge --auto-remove libapache2-mod-php7.2 -y
-sudo rm -rf /etc/php/7.2/apache2
+sudo apt-get purge --auto-remove libapache2-mod-php7.4 -y
+sudo rm -rf /etc/php/7.4/apache2
 sudo rm -rf /etc/apache2
 
 sudo rm /var/www/html/index.html
 
-sudo service php7.2-fpm restart
+sudo service php7.4-fpm restart
 sudo systemctl reload nginx
 
 #node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
 cd ~
 source .bashrc
 
